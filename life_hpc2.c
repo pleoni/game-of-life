e515dd39bd64cc07578ecbee1101b2871325e362
pleoni <ulixes84@yahoo.it>
2014-05-22 20:12:12 +0200

    bugfix

--- life_hpc2.c
+++ life_hpc2.c
@@ -460,7 +460,7 @@ void compute_Borders(double ** grid, double ** next_grid) {
   double neighbors=0.0;
 
   // Compute IntBorders
-  #pragma acc kernels present(grid[nrows+2][ncols+2],next_grid[nrows+2][ncols+2],sum,A[0:ncomp],B[0:ncomp]) async(1)
+  #pragma acc kernels present(grid[0:nrows+2][0:ncols+2],next_grid[0:nrows+2][0:ncols+2],sum,A[0:ncomp],B[0:ncomp]) async(1)
   //#pragma omp parallel
   {
     #pragma omp for private(i,j,k) reduction(+:sum) schedule(static) // collapse(2)
@@ -580,7 +580,7 @@ void compute_Internals(double ** grid, double ** next_grid) {
   double neighbors=0.0;
 
   // Compute Internals
-  #pragma acc kernels present(grid[nrows+2][ncols+2],next_grid[nrows+2][ncols+2],sum,A[0:ncomp],B[0:ncomp]) async(2)
+  #pragma acc kernels present(grid[0:nrows+2][0:ncols+2],next_grid[0:nrows+2][0:ncols+2],sum,A[0:ncomp],B[0:ncomp]) async(2)
   {
     #pragma omp for private(i,j,k) reduction(+:sum) schedule(static)
     #pragma acc loop independent reduction(+:sum) 
