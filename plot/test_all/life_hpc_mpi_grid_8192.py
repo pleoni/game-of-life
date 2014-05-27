4fbff279d84eae2c8a0f3122f29a5c375600973e
pleoni <ulixes84@yahoo.it>
2014-05-27 11:57:25 +0200

    add test_all folder

---
+++ plot/test_all/life_hpc_mpi_grid_8192.py
@@ -0,0 +1,83 @@
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+from numpy import *
+import sys
+import datetime
+
+datafile1="life_acc_mpi.out"
+datafile2="life_host_mpi.out"
+datafile3="life_mic_mpi.out"
+
+if len(sys.argv) > 1:
+    datafile=sys.argv[1]
+
+plotfile="plot_mpi_test.png"
+data1 = loadtxt(datafile1)
+data2 = loadtxt(datafile2)
+data3 = loadtxt(datafile3)
+
+today = datetime.date.today()
+
+fig = plt.figure() # apre una nuova figura
+top    = fig.add_subplot(311)
+middle = fig.add_subplot(312)
+bottom = fig.add_subplot(313)
+
+#############  TOP 
+
+M8ACC    = data1[where((data1[:,0]==8) & (data1[:,2]==8192)    ),:][0] # mpi 8 - grid 8192
+M4ACC    = data1[where((data1[:,0]==4) & (data1[:,2]==8192)    ),:][0] # mpi 4 - grid 8192
+M2ACC    = data1[where((data1[:,0]==2) & (data1[:,2]==8192)    ),:][0] # mpi 2 - grid 8192
+M1ACC    = data1[where((data1[:,0]==1) & (data1[:,2]==8192)    ),:][0] # mpi 1 - grid 8192
+
+top.set_title(str(today) + ' LIFE - MPI on eurora - K20 - grid 8192')
+top.grid()
+top.set_xlabel('comp')
+top.set_ylabel('log(time)')
+#top.set_yscale('log') 
+#top.legend()
+top.set_xlim(-10,1050)
+top.plot(M8ACC[:,5],M8ACC[:,8],'-xr',M4ACC[:,5],M4ACC[:,8],'-+g',M1ACC[:,5],M1ACC[:,8],'-oc');
+
+top.legend(('MPI-8','MPI-4','MPI-1'), loc = 'lower right', shadow = False, prop={'size':8})
+
+
+#############  MIDDLE
+M8HOST    = data2[where((data2[:,0]==8) & (data2[:,2]==8192)    ),:][0] # mpi 8 - grid 8192
+M4HOST    = data2[where((data2[:,0]==4) & (data2[:,2]==8192)    ),:][0] # mpi 4 - grid 8192
+M2HOST    = data2[where((data2[:,0]==2) & (data2[:,2]==8192)    ),:][0] # mpi 2 - grid 8192
+M1HOST    = data2[where((data2[:,0]==1) & (data2[:,2]==8192)    ),:][0] # mpi 1 - grid 8192
+
+middle.set_title(str(today) + ' LIFE - MPI on eurora - HOST - grid 8192')
+middle.grid()
+middle.set_xlabel('comp')
+middle.set_ylabel('log(time)')
+#middle.set_yscale('log') 
+middle.set_xlim(-10,1050)
+
+middle.plot(M8HOST[:,5],M8HOST[:,8],'-xr',M4HOST[:,5],M4HOST[:,8],'-+g',M1HOST[:,5],M1HOST[:,8],'-oc');
+middle.legend(('MPI-8','MPI-4','MPI-1'), loc = 'lower right', shadow = False, prop={'size':8})
+
+
+#############  BOTTOM
+M8MIC    = data3[where((data3[:,0]==8) & (data3[:,2]==8192)    ),:][0] # mpi 8 - grid 8192
+M4MIC    = data3[where((data3[:,0]==4) & (data3[:,2]==8192)    ),:][0] # mpi 4 - grid 8192
+M2MIC    = data3[where((data3[:,0]==2) & (data3[:,2]==8192)    ),:][0] # mpi 2 - grid 8192
+M1MIC    = data3[where((data3[:,0]==1) & (data3[:,2]==8192)    ),:][0] # mpi 1 - grid 8192
+bottom.set_title(str(today) + 'LIFE - MPI on eurora - MIC - grid 8192')
+bottom.grid()
+bottom.set_xlabel('comp')
+bottom.set_ylabel('log(time)')
+#bottom.set_yscale('log') 
+bottom.set_xlim(-10,1050)
+
+bottom.plot(M8MIC[:,5],M8MIC[:,8],'-xr',M4MIC[:,5],M4MIC[:,8],'-+g',M1MIC[:,5],M1MIC[:,8],'-oc');
+bottom.legend(('MPI-8','MPI-4','MPI-1'), loc = 'lower right', shadow = False, prop={'size':8})
+
+plt.subplots_adjust(hspace=0.8)
+
+plt.savefig(plotfile)
+#plt.show()
+
+
---
+++ plot/test_all/life_perf_compilers.py
@@ -0,0 +1,65 @@
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+from numpy import *
+import sys
+import datetime
+
+datafile1="life_host_icc.out"
+datafile2="life_host_gnu.out"
+datafile3="life_host_pgi.out"
+
+if len(sys.argv) > 1:
+    datafile=sys.argv[1]
+
+plotfile="compilers_perf_eurora.png"
+data1 = loadtxt(datafile1)
+data2 = loadtxt(datafile2)
+data3 = loadtxt(datafile3)
+
+today = datetime.date.today()
+
+fig = plt.figure() # apre una nuova figura
+top    = fig.add_subplot(211)
+bottom = fig.add_subplot(212)
+
+#############  TOP 
+
+ICC_C1000 = data1[where((data1[:,0]==1) & (data1[:,5]==1000) ),:][0] # mpi 1 - Comp 1000
+ICC_C0    = data1[where((data1[:,0]==1) & (data1[:,5]==1)    ),:][0] # mpi 1 - comp 0
+
+GNU_C1000 = data2[where((data2[:,0]==1) & (data2[:,5]==1000) ),:][0] # mpi 1 - Comp 1000
+GNU_C0    = data2[where((data2[:,0]==1) & (data2[:,5]==1)    ),:][0] # mpi 1 - comp 0
+
+PGI_C1000 = data3[where((data3[:,0]==1) & (data3[:,5]==1000) ),:][0] # mpi 1 - Comp 1000
+PGI_C0    = data3[where((data3[:,0]==1) & (data3[:,5]==1)    ),:][0] # mpi 1 - comp 0
+
+top.set_title(str(today) + ' life_hpc2 on eurora - NCOMP=1000')
+top.grid()
+top.set_xlabel('lattice Size')
+top.set_ylabel('time')
+#top.set_yscale('log') 
+#top.legend()
+
+top.plot(ICC_C1000[:,3],ICC_C1000[:,8],'-xr',GNU_C1000[:,3],GNU_C1000[:,8],'-xg',PGI_C1000[:,3],PGI_C1000[:,8],'-xc');
+top.legend(('icc','gnu','pgi'), loc = 'upper left', shadow = False, prop={'size':9})
+
+
+#############  BOTTOM 
+
+
+bottom.set_title(str(today) + ' life_hpc2 on eurora - NCOMP=1')
+bottom.grid()
+bottom.set_xlabel('lattice size')
+bottom.set_ylabel('time')
+
+bottom.plot(ICC_C0[:,3],ICC_C0[:,8],'-xr',GNU_C0[:,3],GNU_C0[:,8],'-xg',PGI_C0[:,3],PGI_C0[:,8],'-xc');
+bottom.legend(('icc','gnu','pgi'), loc = 'upper left', shadow = False, prop={'size':9})
+
+
+plt.subplots_adjust(hspace=0.5)
+
+plt.savefig(plotfile)
+#plt.show()
+
+
